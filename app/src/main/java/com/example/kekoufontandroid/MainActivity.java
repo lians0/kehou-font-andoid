c5d4901e6818d73d6ae000ef94b459a1feedd9d2
Lian <913240512@qq.com>
2021-11-26 10:59:48 +0800

    add:添加主代码结构

--- app/src/main/java/com/example/kekoufontandroid/MainActivity.java
+++ app/src/main/java/com/example/kekoufontandroid/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.kekoufontandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends FragmentActivity implements BottomNavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+        initListener();
+    }
+
+    private void initView() {
+        ViewPager2 pagerView = (ViewPager2) findViewById(R.id.pager_view);
+        BottomNavigationView navView = (BottomNavigationView) findViewById(R.id.nav_view);
+    }
+
+    private void initData() {
+    }
+
+    private void initListener() {
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
